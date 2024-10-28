49727921741211f83fc51e1075c5c75d72b88449
krishna3052002 <shukrishna23@gmail.com>
2024-10-28 23:23:36 +0600

    Second Commit

--- Assignment 1/Management.java
+++ Assignment 1/Management.java
@@ -3,6 +3,6 @@ import jdk.swing.interop.SwingInterOpUtils;
 public class Management {
     public static void main(String[] args) {
         System.out.println("Apon Khan");
-
+        System.out.println("Sakib");
     }
 }
